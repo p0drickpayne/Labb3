e4c841d06b54a602c340f182ad50b3e381ee0d66
jakob salomonsson <jakob@salomonssons.se>
2020-06-16 00:11:51 +0200

    vue cli är redo

---
+++ frontend/src/Views/MainView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="content">
+    <div>
+      <p>Hello</p>
+      <p>orivnorlkrvlrlwkvnlwkvewlkvwlekvlwekv</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MainView"
+};
+</script>
+
+<style scoped>
+.content {
+  background-color: orange;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+p {
+  font-size: 72pt;
+  color: black;
+}
+</style>
